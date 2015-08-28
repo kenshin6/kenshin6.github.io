b31bb8b0fa99213e659bdcd5aed05677c98bcff4
kenshin6 <jeffreycgoldman@gmail.com>
2015-08-28 18:58:48 +0900

    still messin with jquery

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 	$(".hello").click(function(){
 		$(this).toggleClass("goodbye hello");
 	});
-	$(".goodbye").mousedown(function(){
-		$(this).draggable();
+	$(".goodbye").draggable();
 	});
 });
 	
