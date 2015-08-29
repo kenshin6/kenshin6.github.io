1763f200cf7340122407954a2745a8dd86a2b54b
kenshin6 <jeffreycgoldman@gmail.com>
2015-08-29 18:59:46 +0900

    missing a bracket

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     if( e.button == 2 ) { 
       $(".goodbye").animate({left: '250px'}, {complete: function(){
       $(".goodbye").hide();
-      }); 
+      }}); 
       return false; 
     } 
     return true; 
